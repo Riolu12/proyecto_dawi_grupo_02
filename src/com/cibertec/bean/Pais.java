9d5ab7e61635d046c324f75cd89813eddfc1e5d4
UsuarioW10 <UsuarioW10@192.168.1.3>
2020-06-22 18:18:22 -0400

    Se agrega la clase Pais

---
+++ src/com/cibertec/bean/Pais.java
@@ -0,0 +1,19 @@
+package com.cibertec.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Pais {
+	
+	
+	
+	private int idPais;
+	
+	
+	private String isoPais;
+	
+	
+	private String nomPais;
+}
